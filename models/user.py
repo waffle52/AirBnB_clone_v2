5a610a541eb517bed812b1cafb7ddffc285a7c69
Nehal Shastri <nhlshstr@gmail.com>
2020-04-02 22:20:29 -0700

    changed nullable values

--- models/user.py
+++ models/user.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
 """This is the user class"""
-from models.base_model import Base
 from models.base_model import BaseModel
 from sqlalchemy.orm import relationship
 from sqlalchemy import Column, String
@@ -18,10 +17,10 @@ class User(BaseModel):
     __tablename__ = "users"
 
     email = Column(String(128), nullable=False)
-    
+
     password = Column(String(128), nullable=False)
-    first_name = Column(String(128), nullable=False)
-    last_name = Column(String(128), nullable=False)
+    first_name = Column(String(128))
+    last_name = Column(String(128))
 
     places = relationship("Place", backref="user", cascade="all, delete")
     reviews = relationship("Review", backref="user", cascade="all, delete")
