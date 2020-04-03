f4f4ef8d2b532024d5f9420cd7d89a7eba671415
Nehal Shastri <nhlshstr@gmail.com>
2020-04-02 22:16:19 -0700

    changed permissions

--- models/user.py
+++ models/user.py
@@ -17,6 +17,7 @@ class User(BaseModel):
     __tablename__ = "users"
 
     email = Column(String(128), nullable=False)
+    
     password = Column(String(128), nullable=False)
     first_name = Column(String(128), nullable=False)
     last_name = Column(String(128), nullable=False)
