87dd00d58e1fa16b834ce26bd9f4882c86ee6258
waffle52 <873@holbertonschool.com>
2020-04-03 06:10:06 +0000

    Added changes to define state

--- models/state.py
+++ models/state.py
@@ -1,11 +1,28 @@
 #!/usr/bin/python3
 """This is the state class"""
-from models.base_model import BaseModel
+import models
+from models.base_model import BaseModel, Base
+from sqlalchemy import Column, String
+from sqlalchemy.orm import relationship
+from os import getenv
 
 
-class State(BaseModel):
+class State(BaseModel, Base):
     """This is the class for State
     Attributes:
         name: input name
     """
-    name = ""
+    __tablename__ = 'states'
+    name = Column(String(128), nullable=False)
+    if getenv("HBNB_TYPE_STORAGE") == 'db':
+        cities = relationship('City', backref='state', cascade='all,
+                              delete-orphan')
+    else:
+        @property
+        def cities(self):
+            list = []
+            for x in models.storage.all().values():
+                if self.id == x.state_id:
+                    list.append(x)
+
+            return (list)
