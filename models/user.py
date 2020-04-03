b80b4e3463d87d444d9fa2cbf6d47a01a83bd019
Nehal Shastri <nhlshstr@gmail.com>
2020-04-02 22:35:33 -0700

    added task 7 again, from ground up

--- models/user.py
+++ models/user.py
@@ -1,11 +1,11 @@
 #!/usr/bin/python3
 """This is the user class"""
 from models.base_model import BaseModel, Base
-from sqlalchemy.orm import relationship
 from sqlalchemy import Column, String
+from sqlalchemy.orm import relationship
 
 
-class User(BaseModel):
+class User(BaseModel, Base):
     """This is the class for user
     Attributes:
         email: email address
@@ -13,14 +13,13 @@ class User(BaseModel):
         first_name: first name
         last_name: last name
     """
-
     __tablename__ = "users"
 
     email = Column(String(128), nullable=False)
     password = Column(String(128), nullable=False)
-    first_name = Column(String(128), nullable=False)
-    last_name = Column(String(128), nullable=False)
+    first_name = Column(String(128), nullable=True)
+    last_name = Column(String(128), nullable=True)
 
-    places = relationship("Place", backref="user", cascade="all, delete-orphan")
-    reviews = relationship("Review", backref="user", cascade="all,
-                           delete-orphan")
+    places = relationship("Place", backref="user", cascade="all,delete-orphan")
+    reviews = relationship("Review", backref="user",
+                           cascade="all,delete-orphan")
