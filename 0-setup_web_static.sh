f779c6ff9c572696924f9b74c265fcd45c5ab23d
Nehal Shastri <nhlshstr@gmail.com>
2020-04-06 22:29:01 -0700

    chmod

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -7,13 +7,11 @@ apt-get -y install nginx
 ufw allow 'Nginx HTTP'
 
 mkdir -p /data/web_static/releases/test /data/web_static/shared
-
 echo "Holberton School" > /data/web_static/releases/test/index.html
 
 ln -sf /data/web_static/releases/test /data/web_static/current
 
 chown -R ubuntu:ubuntu /data
-
 sed -i '/listen 80 default_server;/a location /hbnb_static/ { alias /data/web_static/current/; }' /etc/nginx/sites-available/default
 
 service nginx restart
