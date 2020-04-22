2a5cd0921a643a0907e3cc924406e7902767ccb8
waffle52 <873@holbertonschool.com>
2020-04-22 23:25:41 +0000

    Uploading for testing

---
+++ web_flask/6-number_odd_or_even.py
@@ -0,0 +1,49 @@
+#!/usr/bin/python3
+""" Script to start a Flask Web App """
+from flask import Flask
+from flask import render_template
+
+app = Flask(__name__)
+app.url_map.strict_slashes = False
+
+
+@app.route('/')
+def hello_world():
+    return 'Hello HBNB!'
+
+
+@app.route('/hbnb')
+def hbnb_display():
+    return 'HBNB'
+
+
+@app.route('/c/<text>')
+def c_display(text):
+    return 'C {}'.format(text.replace("_", " "))
+
+
+@app.route('/python')
+@app.route('/python/<text>')
+def py_display(text='is cool'):
+    return ('Python {}'.format(text.replace("_", " ")))
+
+
+@app.route('/number/<int:n>')
+def n_display(n):
+    return ("{} is a number".format(n))
+
+
+@app.route('/number_template/<int:n>')
+def temp(n):
+    return render_template('5-number.html', num=n)
+
+
+@app.route('/number_odd_or_even/<int:n>')
+def oddEven(n):
+    word = "even"
+    if (n % 2 == 1):
+        word = "odd"
+    return render_template('6-number_odd_or_even.html', num=n, set=word)
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=5000)
