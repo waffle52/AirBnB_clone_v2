1deb45203bef59f9053bb75f9933ce558c4d1bfb
waffle52 <873@holbertonschool.com>
2020-04-22 06:03:57 +0000

    Removed newlines

--- web_flask/1-hbnb_route.py
+++ web_flask/1-hbnb_route.py
@@ -8,11 +8,11 @@ app.url_map.strict_slashes = False
 
 @app.route('/')
 def hello_world():
-    return 'Hello HBNB!\n'
+    return 'Hello HBNB!'
 
 
 @app.route('/hbnb')
 def hbnb_display():
-    return 'HBNB\n'
+    return 'HBNB'
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=5000)
