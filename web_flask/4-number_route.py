6478e63aeb1bdfefb175badba86d6298472da878
waffle52 <873@holbertonschool.com>
2020-04-22 22:35:57 +0000

    Uploading for testing

---
+++ web_flask/4-number_route.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python3
+""" Script to start a Flask Web App """
+from flask import Flask
+import re
+
+app = Flask(__name__)
+app.url_map.strict_slashes = False
+
+
+@app.route('/')
+def hello_world():
+    return 'Hello HBNB!'
+
+
+@app.route('/hbnb')
+def hbnb_display():
+    return 'HBNB'
+
+
+@app.route('/c/<text>')
+def c_display(text):
+    return 'C {}'.format(text.replace("_", " "))
+
+
+@app.route('/python')
+@app.route('/python/<text>')
+def py_display(text='is cool'):
+    return ('Python {}'.format(text.replace("_", " ")))
+
+
+@app.route('/number/<int:n>')
+def n_display(n):
+    return ("{} is a number".format(n))
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=5000)
