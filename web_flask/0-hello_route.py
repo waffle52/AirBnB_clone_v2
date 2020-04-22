d2b919dfaad70d0194f0258773176c90d7d0099e
waffle52 <873@holbertonschool.com>
2020-04-22 05:53:27 +0000

    added check to see if main

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -9,4 +9,5 @@ app.url_map.strict_slashes = False
 @app.route('/')
 def hello_world():
     return 'Hello HBNB!'
-app.run(host='0.0.0.0', port=5000)
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=5000)
